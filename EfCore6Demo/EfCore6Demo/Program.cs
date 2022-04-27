6fd8d37db3bc7c0ac273992e35c8f556c5cb350d
Sebastien Bonelli <sebastien.bonelli@external.technipenergies.com>
2022-04-27 18:14:38 +0200

    implement EFCore6

--- EfCore6Demo/EfCore6Demo/Program.cs
+++ EfCore6Demo/EfCore6Demo/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
 Console.WriteLine("Hello, World!");
+
+
+using (var context = new BloggingContext())
+{
+    var blog = context.Blogs.Include(m => m.Posts);
+}
+
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            @"Server=(localdb)\mssqllocaldb;Database=Blogging;Trusted_Connection=True");
+    }
+}
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string Url { get; set; }
+    public int Rating { get; set; }
+    public List<Post> Posts { get; set; }
+}
+
+public class Post
+{
+    public int PostId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    public int BlogId { get; set; }
+    public Blog Blog { get; set; }
+}
+
+
